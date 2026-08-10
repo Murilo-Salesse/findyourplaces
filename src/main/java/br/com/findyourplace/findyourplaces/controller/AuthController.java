b540a2723ec40f11710a08ee031febc6d4d3bdc3
Murilo-Salesse <salessedev@gmail.com>
2026-08-10 18:23:18 -0300

    feat(add): stating authentication

--- src/main/java/br/com/findyourplace/findyourplaces/controller/AuthController.java
+++ src/main/java/br/com/findyourplace/findyourplaces/controller/AuthController.java
@@ -1,5 +1,10 @@
 package br.com.findyourplace.findyourplaces.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
 public class AuthController {
 
 }
